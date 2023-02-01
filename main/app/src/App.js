39ca7ed01781d91c84d8d91db1534152ac662d21
SMITESH AROLKAR <smitesharolkar99@gmail.com>
2023-02-01 16:38:32 +0530

    Making a Counter

--- main/app/src/App.js
+++ main/app/src/App.js
@@ -1,70 +1,61 @@
-
 import { useState } from "react";
 import "./App.css";
 
+const App = () => {
+  const [counter, setCounter] = useState(0);
 
-function App(props) {
-  //Data
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-  console.log(now, a + b);
-  const repo = "https://github.com/SmiteshArolkar/ReactJs"
-  const [counter , setCounter ] = useState(0);
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
 
-  const setZero = () => setCounter(0)
-
-  setTimeout(() => setCounter(counter+1),1000)
-  
-  return (
-
-    <div className="App">
-      
-      <button onClick={handleCheck()}>
-      <h3>
-        {counter}
-      </h3>
-      </button>
-      <div>
-        <button>
-          Reset
-        </button>
-      </div>
-    </div>
-    
-  
-  );
-}
-
-const Hello = (props) => {
-  const born_year = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
-  }
   return (
     <div>
-      <p>Hello {props.name} You are {props.age} old.</p>
-      <p>So you were born in {born_year()}</p>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </div>
   );
 };
 
-const Footer = (props) => {
-  
-  return (
-    <div>
-      <h4>
-        Page Created By Smitesh Repo  
-        <a href = {props.link} > Link</a>
-      </h4>
-    </div>
-  )
-}
+const Display = (props) => {
+  return <div>{props.counter}</div>;
+};
 
-//event handler
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
+};
 
-const handleCheck = () => {
-  console.log("clicked")
-}
+// const Hello = (props) => {
+//   const born_year = () => {
+//     const yearNow = new Date().getFullYear();
+//     return yearNow - props.age;
+//   };
+//   return (
+//     <div>
+//       <p>
+//         Hello {props.name} You are {props.age} old.
+//       </p>
+//       <p>So you were born in {born_year()}</p>
+//     </div>
+//   );
+// };
+
+// const Footer = (props) => {
+//   return (
+//     <div>
+//       <h4>
+//         Page Created By Smitesh Repo
+//         <a href={props.link}> Link</a>
+//       </h4>
+//     </div>
+//   );
+// };
+
+// //event handler
+
+// const handleCheck = () => {
+//   console.log("clicked");
+// };
 
 export default App;
